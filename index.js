6fb4286f7d275cc5e978afe644f65fa2e5c6f436
Xxeet7 <2good4u991@gmail.com>
2025-07-11 16:12:06 +0700

    docs: add simple explanation

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const { Command } = require("commander")
 const crypto = require("crypto")
 const program = new Command()
 
+// var assigning
 const lowercase = "abcdefghijklmnopqrstuvwxyz"
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numbers = "0123456789"
 const symbols = "!@#$%^&*()"
 let pools = ""
 
+// assigning program version and description
 program
     .version("1.0.0", "-v, --version", "output the version number")
     .description("secure password generator with options on the terminal")
 
+// program options and action
 program
     .option("-l, --length [number]", "length of the password (default: 8)", "8")
     .option("-n, --numbers", "add numbers to the password")
@@ -28,6 +31,7 @@ program
     )
     .action((options) => {
 
+        // check user option to assign pools
         if (options.uppercase && options.lowercase) {
             pools = lowercase + uppercase
         } else if (options.uppercase) {
@@ -40,17 +44,20 @@ program
             )
         }
 
+        // for number and symbols options
         if (options.numbers) pools += numbers
         if (options.symbols) pools += symbols
 
         const chars = pools.split("")
         let password = ""
 
+        // making the password
         for (let i = 0; i < Number(options.length); i++) {
             const index = crypto.randomInt(0, chars.length)
             password += chars[index]
         }
 
+        // log it out
         console.log(password)
     })
 
